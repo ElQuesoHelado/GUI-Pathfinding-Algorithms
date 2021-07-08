a5993954ef8fec186b1a056ad59ce98a66309aff
jjac1999 <josealvacornejo1@gmail.com>
2021-07-07 21:13:41 -0500

    Additional Dijsktra implementation features and tests

--- src/main/java/GUIapplication/PathFinding.java
+++ src/main/java/GUIapplication/PathFinding.java
@@ -45,14 +45,19 @@ public final class PathFinding {
     private PathFinding() {
     }
 
-    public static void dijkstra(AdjList adjList, String startNode, String endNode) {
+    public static int dijkstra(AdjList adjList, String startNode, String endNode, LinkedList<String> pathList) {
         //Hashtable used to store minimal distance from startNode
         HashMap<String, Integer> d = new HashMap<>(adjList.size());
-        PriorityQueue<Entry> q = new PriorityQueue<>(adjList.size()); //Priority queue for storing current shortest path
+        //Priority queue for storing current shortest path
+        PriorityQueue<Entry> q = new PriorityQueue<>(adjList.size());
+
+        //Hashtable used for tracking path of vertexes, each key node has of value the previous node of shortest path
+        HashMap<String, String> previousVertexes = new HashMap<>(adjList.size());
 
         for (String key : adjList.getKeySet()) { //Sets all initial distances to infinity
             d.putIfAbsent(key, Integer.MAX_VALUE);
             q.add(new Entry(Integer.MAX_VALUE, key));
+            previousVertexes.putIfAbsent(key, null);
         }
 
         //Set source node value to 0
@@ -60,26 +65,45 @@ public final class PathFinding {
         q.remove(new Entry(Integer.MAX_VALUE, startNode));
         q.add(new Entry(0, startNode));
 
-        Pair<String, Integer>[] neighbors;
+        //Helper variables
+        Pair<String, Integer>[] neighbors;//Array of pairs <neighborKey, edgeWeight>
+        String u, //Current Node with shortest path / top of min-queue
+                neighbor;
+        int edgeWeight;
         while (q.size() != 0) {
-            String u = q.poll().getValue();
+            u = q.poll().getValue();
             neighbors = adjList.getNeighbors(u);
-            for (Pair<String, Integer> pair : neighbors) {//Loops through every neighbor to find a shorter path
+            for (Pair<String, Integer> neighborKeyValuePair : neighbors) {//Loops through every neighbor
+                neighbor = neighborKeyValuePair.getKey();
+                edgeWeight = neighborKeyValuePair.getValue();
                 //Relaxation step
                 //Checks if distance to neighbor node is less than the min distance already found
-                if (d.get(pair.getKey()) > d.get(u) + pair.getValue()) {
-                    d.put(pair.getKey(), d.get(u) + pair.getValue()); //Assign shorter path found to neighbor
+                //Checks if distance to current node is infinite, to avoid overflow
+                if ((d.get(u) != Integer.MAX_VALUE) && (d.get(neighbor) > d.get(u) + edgeWeight)) {
                     //Decreases the key for neighbor in queue
-                    q.remove(new Entry(Integer.MAX_VALUE, startNode));
-                    q.add(new Entry(0, startNode));
-                }
+                    q.remove(new Entry(d.get(neighbor), neighbor));//Removes and adds to prompt a min-queue restructure
+                    q.add(new Entry(d.get(u) + edgeWeight, neighbor));
 
+                    //Assign shorter path found to neighbor in out array
+                    d.put(neighbor, d.get(u) + edgeWeight);
+
+                    //Assigns previous shortest node to neighbor
+                    previousVertexes.put(neighbor, u);
+                }
             }
+
             //endNode found
-            if (Objects.equals(u, endNode))
-                break;
+            if (Objects.equals(u, endNode)) {
+                //Build path of vertexes
+                String current = endNode;
+                pathList.add(current);
+                while ((current = previousVertexes.get(current)) != null) {
+                    pathList.addFirst(current);
+                }
+                return d.get(u);
+            }
 
         }
-
+        return Integer.MAX_VALUE;
     }
 }
--- test/java/GUIapplication/PathFindingTest.java
+++ test/java/GUIapplication/PathFindingTest.java
@@ -2,11 +2,26 @@ package GUIapplication;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PathFindingTest {
 
     @Test
     void dijkstra() {
+        AdjList list = new AdjList();
+        LinkedList<String> pathList;
+        LinkedList<String> expectedPathList;
+
+        pathList = new LinkedList<>();
+        list.processEdgeListFile("test/java/GUIapplication/premadeGraph.txt");
+        int path = PathFinding.dijkstra(list, "55T", "55C", pathList);
+        assertEquals(19, path);
+
+        expectedPathList = new LinkedList<>(List.of("55T", "55Q", "55N", "55I", "55G", "55B", "55A", "55C"));
+        assertEquals(expectedPathList, pathList);
     }
 }
